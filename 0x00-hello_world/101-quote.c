3cbdc266cae32f1ed4af0bbf374754cb18beeb86
Joshua <joshuaakototamakloe@gmail.com>
2023-06-16 07:09:51 -0700

    101n-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <umistd.h>
 /**
- * main - A program to print "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a newline, to the standard error.
- * Return: Always 1
+ * main - Print a write function
+ * Return: always (1)
  */
 
 int main(void)
 {
-      write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);	
-      return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
