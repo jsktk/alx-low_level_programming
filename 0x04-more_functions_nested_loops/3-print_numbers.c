824aabbfa102bd588d0a4bba8506562d886b29b8
Joshua <joshuaakototamakloe@gmail.com>
2023-06-22 13:05:15 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The number s since 0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	putchar(x + '0');
+	}
+	putchar('\n');
+}
